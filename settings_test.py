4bb5f4ee7f23c1d5a3f3007802f7032aaa521da9
alexandermendes <alexanderhmendes@gmail.com>
2016-01-26 18:27:27 +0000

    Add test

---
+++ settings_test.py
@@ -0,0 +1,2 @@
+Z3950_DATABASES = {'loc': {'host': 'z3950.loc.gov', 'db': 'Voyager',
+                           'port': '7090'}}
--- tests/__init__.py
+++ tests/__init__.py
@@ -7,10 +7,20 @@ import pybossa_z3950 as plugin
 # Use the PyBossa test suite
 sys.path.append(os.path.abspath("./pybossa/test"))
 
+from default import Test
+
 
 def setUpPackage():
     """Setup the plugin."""
     from default import flask_app
     with flask_app.app_context():
+        settings = os.path.abspath('./settings_test.py')
+        flask_app.config.from_pyfile(settings)
         plugin_dir = os.path.dirname(plugin.__file__)
         plugin.PyBossaZ3950(plugin_dir).setup()
+
+
+class TestPlugin(Test):
+
+    def test_blueprint_registered(self):
+        assert 'z3950' in self.flask_app.blueprints
